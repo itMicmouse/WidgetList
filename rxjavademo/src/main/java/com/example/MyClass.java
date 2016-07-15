7b9d3d747b54f7d93c92e8aa69a8f6c30ea8b5e4
361176335@qq.com <yang900713>
2016-07-15 16:58:24 +0800

    add RxJava SimpleDemo

---
+++ rxjavademo/src/main/java/com/example/MyClass.java
@@ -0,0 +1,46 @@
+package com.example;
+
+import rx.Observable;
+import rx.Subscriber;
+
+/**
+ * 创建型操作符
+ */
+public class MyClass {
+    public static void main(String[] args) {
+        create();
+    }
+
+    public static void create(){
+        Observable.create(new Observable.OnSubscribe<Integer>() {
+            @Override
+            public void call(Subscriber<? super Integer> observer) {
+                try {
+                    if (!observer.isUnsubscribed()) {
+                        for (int i = 1; i < 5; i++) {
+                            observer.onNext(i);
+                        }
+                        observer.onCompleted();
+                    }
+                } catch (Exception e) {
+                    observer.onError(e);
+                }
+            }
+        } ).subscribe(new Subscriber<Integer>() {
+            @Override
+            public void onNext(Integer item) {
+                System.out.println("Next: " + item);
+            }
+
+            @Override
+            public void onError(Throwable error) {
+                System.err.println("Error: " + error.getMessage());
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("Sequence complete.");
+            }
+        });
+    }
+}
