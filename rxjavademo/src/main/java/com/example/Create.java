38fda4f52262566e455318e41726aaa5b9974468
361176335@qq.com <yang900713>
2016-07-29 15:34:34 +0800

    delete androidjs and add some Rxjava demo

---
+++ rxjavademo/src/main/java/com/example/Create.java
@@ -0,0 +1,260 @@
+package com.example;
+
+import java.util.concurrent.TimeUnit;
+import java.util.zip.Inflater;
+
+import rx.Observable;
+import rx.Subscriber;
+import rx.functions.Action0;
+import rx.functions.Action1;
+import rx.functions.Func0;
+import rx.functions.Func1;
+import rx.functions.Func2;
+
+/**
+ * 创建型操作符
+ */
+public class Create {
+    public static void main(String[] args) {
+//        create();
+//        from();
+//        just();
+//        defer();
+//        timer();
+//        interval();
+//        repeat();
+        repeatWhen();
+    }
+
+
+    public static void create() {
+        Observable.create(new Observable.OnSubscribe<Integer>() {
+            @Override
+            public void call(Subscriber<? super Integer> observer) {
+                try {
+                    if (!observer.isUnsubscribed()) {
+                        for (int i = 1; i < 5; i++) {
+                            observer.onNext(i);
+                        }
+                        observer.onCompleted();
+                    }
+                } catch (Exception e) {
+                    observer.onError(e);
+                }
+            }
+        }).subscribe(new Subscriber<Integer>() {
+            @Override
+            public void onNext(Integer item) {
+                System.out.println("Next: " + item);
+            }
+
+            @Override
+            public void onError(Throwable error) {
+                System.err.println("Error: " + error.getMessage());
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("Sequence complete.");
+            }
+        });
+    }
+
+    private static int i;
+    public static void defer() {
+        i = 10;
+        Observable justObservable = Observable.just(i);
+        i = 12;
+        Observable deferObservable = Observable.defer(new Func0<Observable<Object>>() {
+            @Override
+            public Observable<Object> call() {
+                return Observable.just(i);
+            }
+        });
+        i = 15;
+
+        justObservable.subscribe(new Subscriber() {
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(Object o) {
+                System.out.println("just result:" + o.toString());
+            }
+        });
+
+        deferObservable.subscribe(new Subscriber() {
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(Object o) {
+                System.out.println("defer result:" + o.toString());
+            }
+        });
+    }
+
+    public static void from() {
+        Integer[] items = {0, 1, 2, 3, 4, 5};
+        Observable myObservable = Observable.from(items);
+
+        myObservable.subscribe(
+                new Action1<Integer>() {
+                    @Override
+                    public void call(Integer item) {
+                        System.out.println(item);
+                    }
+                },
+                new Action1<Throwable>() {
+                    @Override
+                    public void call(Throwable error) {
+                        System.out.println("Error encountered: " + error.getMessage());
+                    }
+                },
+                new Action0() {
+                    @Override
+                    public void call() {
+                        System.out.println("Sequence complete");
+                    }
+                }
+        );
+    }
+
+    public static void just() {
+        Observable.just(1, 2, 3, 4).subscribe(new Subscriber<Integer>() {
+            @Override
+            public void onCompleted() {
+                System.out.println("Sequence complete");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                System.out.println(e.getMessage());
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                System.out.println(integer);
+            }
+        });
+    }
+
+    public static void timer() {
+        //每隔两秒产生一个数字
+        Observable.timer(2, 2, TimeUnit.SECONDS).subscribe(new Subscriber<Long>() {
+            @Override
+            public void onCompleted() {
+                System.out.println("Sequence complete.");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                System.out.println("error:" + e.getMessage());
+            }
+
+            @Override
+            public void onNext(Long aLong) {
+                System.out.println("Next:" + aLong.toString());
+            }
+        });
+
+        try {
+            Thread.sleep(20000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void interval(){
+        Observable.range(3,10).subscribe(new Subscriber<Integer>() {
+            @Override
+            public void onCompleted() {
+                System.out.println("onCompleted");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                System.out.println("onError");
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                System.out.println(integer);
+            }
+        });
+    }
+
+    public static void repeat(){
+        Observable.range(3,5).repeat(2).subscribe(new Subscriber<Integer>() {
+            @Override
+            public void onCompleted() {
+                System.out.println("onCompleted");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                System.out.println("onError");
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                System.out.println(integer);
+            }
+        });
+    }
+    public static void repeatWhen(){
+        Observable.just(1,2,3).repeatWhen(new Func1<Observable<? extends Void>, Observable<?>>() {
+            @Override
+            public Observable<?> call(Observable<? extends Void> observable) {
+                //重复3次
+                return observable.zipWith(Observable.range(1, 3), new Func2<Void, Integer, Integer>() {
+                    @Override
+                    public Integer call(Void aVoid, Integer integer) {
+                        return integer;
+                    }
+                }).flatMap(new Func1<Integer, Observable<?>>() {
+                    @Override
+                    public Observable<?> call(Integer integer) {
+                        System.out.println("delay repeat the " + integer + " count");
+                        //1秒钟重复一次
+                        return Observable.timer(1, TimeUnit.SECONDS);
+                    }
+                });
+            }
+        }).subscribe(new Subscriber<Integer>() {
+            @Override
+            public void onCompleted() {
+                System.out.println("Sequence complete.");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                System.err.println("Error: " + e.getMessage());
+            }
+
+            @Override
+            public void onNext(Integer value) {
+                System.out.println("Next:" + value);
+            }
+        });
+        try {
+            Thread.sleep(20000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
