02095acd45ea9e40df7dde56ad97a5f4484e3782
361176335@qq.com <yang900713>
2016-08-22 12:54:28 +0800

    sqldelight demo

--- appdagger2/src/main/java/org/micmource/appdagger2/MainActivity.java
+++ appdagger2/src/main/java/org/micmource/appdagger2/MainActivity.java
@@ -1,12 +1,10 @@
 package org.micmource.appdagger2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
 
 import org.micmource.appdagger2.adapter.UserAdapter;
-import org.micmource.appdagger2.module.UserModule;
 
 import javax.inject.Inject;
 
@@ -20,12 +18,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ListView lv_list = (ListView) findViewById(R.id.lv_list);
-
-        // 完成注入
-//        DaggerUserComponent.builder()
-//                .userModule(new UserModule(this))
-//                .build()
-//                .inject(this);
 
 
         lv_list.setAdapter(adapter);
