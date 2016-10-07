4a077a4f432617f94b1e4e95fe7683690ed6ed18
yakun <361176335@qq.com>
2016-10-07 21:21:10 +0800

    sql

--- realmreserve/src/main/java/org/micmource/realmreserve/RealmeFragment.java
+++ realmreserve/src/main/java/org/micmource/realmreserve/RealmeFragment.java
@@ -43,53 +43,6 @@ public class RealmeFragment extends Fragment {
     }
 
     public void addData() {
-        // Use them like regular java objects
-        Dog dog = new Dog();
-        dog.setName("Rex");
-        dog.setAge(1);
-
-        // Create a RealmConfiguration that saves the Realm file in the app's "files" directory.
-        RealmConfiguration realmConfig = new RealmConfiguration.Builder(getActivity()).build();
-        Realm.setDefaultConfiguration(realmConfig);
-
-        // Get a Realm instance for this thread
-        Realm realm = Realm.getDefaultInstance();
-
-// Query Realm for all dogs younger than 2 years old
-        final RealmResults<Dog> puppies = realm.where(Dog.class).lessThan("age", 2).findAll();
-        puppies.size(); // => 0 because no dogs have been added to the Realm yet
-
-// Persist your data in a transaction
-        realm.beginTransaction();
-        final Dog managedDog = realm.copyToRealm(dog); // Persist unmanaged objects
-        Person person = realm.createObject(Person.class); // Create managed objects directly
-        person.getDogs().add(managedDog);
-        realm.commitTransaction();
-
-// Listeners will be notified when data changes
-        puppies.addChangeListener(new RealmChangeListener<RealmResults<Dog>>() {
-            @Override
-            public void onChange(RealmResults<Dog> results) {
-                // Query results are updated in real time
-                puppies.size(); // => 1
-            }
-        });
-
-// Asynchronously update objects on a background thread
-        realm.executeTransactionAsync(new Realm.Transaction() {
-            @Override
-            public void execute(Realm bgRealm) {
-                Dog dog = bgRealm.where(Dog.class).equalTo("age", 1).findFirst();
-                dog.setAge(3);
-            }
-        }, new Realm.Transaction.OnSuccess() {
-            @Override
-            public void onSuccess() {
-                // Original queries and Realm objects are automatically updated.
-                puppies.size(); // => 0 because there are no more puppies younger than 2 years old
-                managedDog.getAge();   // => 3 the dogs age is updated
-            }
-        });
 
     }
 }
